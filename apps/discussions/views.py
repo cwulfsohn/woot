b9cd484ea2692425bc3d876a90cf6a192c4d1b65
Curtis Wulfsohn <cwulfsohn26@gmail.com>
2017-01-30 13:17:06 -0600

    discussion update

--- apps/discussions/views.py
+++ apps/discussions/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, redirect, reverse
 from .models import *
+from ..login.models import *
+from ..home.models import *
 
-def index(request):
-    return render(request, 'discussions/index.html')
+def discussion(request):
+    print request.session['id']
+    user = User.objects.get(id = request.session["id"])
+    context = {
+        'user':user
+    }
+    return render(request, 'discussions/index.html', context)
 
 def dummy(request):
-    
+
     return redirect(reverse('home:dummy'))
