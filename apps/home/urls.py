786fcc084b88bca30b4d1198389ebd221f10db88
Mick Johnson <mickajohnson@outlook.com>
2017-01-30 15:59:16 -0600

    Added add product page

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^dummy/$', views.dummy, name="dummy"),
-    url(r'^product/add/$', views.add_product, name="add_product")
+    url(r'^product/new/$', views.new_product, name="new_product"),
+    url(r'^product/new/add/$', views.add_product, name="add_product"),
+    url(r'^product/new/image/(?P<id>\d+)$', views.new_image, name="new_image"),
+    url(r'^product/new/image/(?P<id>\d+)/upload$', views.upload_image, name="upload_image"),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 from .models import *
+from .forms import ImageUploadForm
+from datetime import datetime
 
 # Create your views here.
 def index(request):
@@ -9,10 +11,42 @@ def dummy(request):
 
     return redirect(reverse('home:index'))
 
-def add_product(request):
-
+def new_product(request):
     context = {
             "categories": Category.objects.all(),
             "subcategories": Subcategory.objects.all(),
     }
-    return render(request, 'home/add_product.html')
+    return render(request, 'home/new_product.html', context)
+
+def add_product(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        description = request.POST["description"]
+        subcategory = Subcategory.objects.get(subcategory=request.POST["subcategory"])
+        price = request.POST["price"]
+        list_price = request.POST["list_price"]
+        active = request.POST["active"]
+        daily_deal = request.POST["daily_deal"]
+        quantity = request.POST["quantity"]
+        expire_date = datetime.strptime(request.POST["expire_date"], "%m/%d/%Y")
+        product = Product.objects.create(name=name, description=description, subcategory=subcategory, price=price, list_price=list_price, active=active, daily_deal=daily_deal, quantity=quantity, expire_date=expire_date, rating=3)
+
+        return redirect(reverse('home:new_image', kwargs={'id':product.id}))
+    else:
+        return redirect(reverse('home:new_product'))
+
+def new_image(request, id):
+    product = Product.objects.get(id=id)
+    context = {
+            "product":product
+    }
+    return render(request, 'home/new_image.html', context)
+
+def upload_image(request, id):
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = Product.objects.get(id=id)
+            m = Image.objects.create(product=product, image = form.cleaned_data['image'])
+            return HttpResponse('image upload success')
+    return redirect(reverse('home:new_image', kwargs={'id':id}))
