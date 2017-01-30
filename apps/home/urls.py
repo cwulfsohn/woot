0fb4597ead890cd6beae30053836df0b825de509
Mick Johnson <mickajohnson@outlook.com>
2017-01-30 17:05:47 -0600

    Fixed picture upload

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^dummy/$', views.dummy, name="dummy"),
+    url(r'^product/show/(?P<id>\d+)$', views.show_product, name="show_product"),
     url(r'^product/new/$', views.new_product, name="new_product"),
     url(r'^product/new/add/$', views.add_product, name="add_product"),
     url(r'^product/new/image/(?P<id>\d+)$', views.new_image, name="new_image"),
