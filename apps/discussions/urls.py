212a0815e967f66d4e285d466f3eaaa183835441
Curtis Wulfsohn <cwulfsohn26@gmail.com>
2017-01-31 14:59:49 -0600

    move discussion into home

--- apps/discussions/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^product/(?P<id>\d+)$', views.discussion, name="discussion"),
-    url(r'^comment$', views.comment, name="comment"),
-]
--- woot/settings.py
+++ woot/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.discussions',
     'apps.checkout',
     'apps.login',
     'apps.home',
