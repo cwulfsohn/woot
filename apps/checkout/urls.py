34f26e6aacd1eeb4da47699d29d91a93b3ac722c
Mick Johnson <mickajohnson@outlook.com>
2017-01-31 19:08:04 -0600

    Started checkout

--- apps/checkout/urls.py
+++ apps/checkout/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^add_cart/$', views.add_cart, name="add_cart"),
+    url(r'^add_cart/(?P<id>\d+)$', views.add_cart, name="add_cart"),
 ]
--- apps/checkout/views.py
+++ apps/checkout/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, redirect, reverse
+from ..home.models import Product
 
 def index(request):
     return render(request, 'checkout/index.html')
 
-def add_cart(request):
-    if request.method == "POST":
-        pass
-    return redirect(reverse('home:show'))
+def add_cart(request, id):
+    pass
+    # if request.method == "POST":
+    #     quantity = request.POST['quantity']
+    #     quantity = int(quantity)
+    #     if not "cart" in request.session or not request.session['cart']:
+    #         request.session["cart"] = [id]
+    #     cart = request.session["cart"]
+    #     # for quantity in range(quantity):
+    #     cart = cart.append(id)
+    #     for die in request.session["cart"]:
+    #         print die
+    #     request.session["cart"] = cart
+    # return redirect(reverse('home:show_product', kwargs={'id':id}))
