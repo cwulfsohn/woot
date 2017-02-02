34a9426ab53c8279590fa14dc3a003f66c6c6421
Mick Johnson <mickajohnson@outlook.com>
2017-02-01 20:24:14 -0600

    Added address and card to checkout

--- apps/checkout/urls.py
+++ apps/checkout/urls.py
@@ -3,5 +3,15 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^buy/$', views.buy, name="buy"),
+    url(r'^address/$', views.address, name="address"),
+    url(r'^billing/$', views.billing, name="billing"),
+    url(r'^billing/add/$', views.add_card, name="add_card"),
+    url(r'^address/add$', views.add_address, name="add_address"),
+    url(r'^address/select$', views.select_address, name="select_address"),
+    url(r'^billing/select$', views.select_card, name="select_card"),
     url(r'^add_cart/(?P<id>\d+)$', views.add_cart, name="add_cart"),
+    url(r'^remove/(?P<id>\d+)$', views.remove, name="remove"),
+
+
 ]
